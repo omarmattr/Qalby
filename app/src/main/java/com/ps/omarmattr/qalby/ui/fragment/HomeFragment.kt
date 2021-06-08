d32c845a6219972e38ff7771f73dbc91f4963794
omarmattr <omarabomattr@gmail.com>
2021-06-08 18:26:36 +0300

    set count down timer on HomeFragment.kt

--- app/src/main/java/com/ps/omarmattr/qalby/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/ps/omarmattr/qalby/ui/fragment/HomeFragment.kt
@@ -8,10 +8,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ps.omarmattr.qalby.databinding.FragmentHomeBinding
 import com.ps.omarmattr.qalby.model.location.ResultLocation
+import com.ps.omarmattr.qalby.model.solahTime.SendParam
+import com.ps.omarmattr.qalby.model.solahTime.SolahTime
+import com.ps.omarmattr.qalby.other.FunctionConstant.addSolah
 import com.ps.omarmattr.qalby.ui.viewmodel.MainViewModel
+import com.ps.omarmattr.qalby.ui.viewmodel.SolahViewModel
 import com.ps.omarmattr.qalby.util.ResultRequest
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
@@ -20,12 +23,17 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class HomeFragment:Fragment() {
-    private val mBinding by lazy{
+class HomeFragment : Fragment() {
+    private val mBinding by lazy {
         FragmentHomeBinding.inflate(layoutInflater)
     }
+
     @Inject
-    lateinit var viewModel: MainViewModel
+    lateinit var mainViewModel: MainViewModel
+
+    @Inject
+    lateinit var viewModel: SolahViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,21 +43,89 @@ class HomeFragment:Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         CoroutineScope(Dispatchers.IO).launch {
-        viewModel.getLocationLiveData.collect {
-            withContext(Dispatchers.Main) {
-            when(it.status){
-                ResultRequest.Status.EMPTY->{}
-                ResultRequest.Status.SUCCESS->{
-                    val data = it.data as ResultLocation
-                    mBinding.txtLocation.text = data.address.country +" , " +data.address.city
-
+            mainViewModel.getLocationLiveData.collect {
+                withContext(Dispatchers.Main) {
+                    when (it.status) {
+                        ResultRequest.Status.EMPTY -> {
+                        }
+                        ResultRequest.Status.SUCCESS -> {
+                            val data = it.data as ResultLocation
+                            mBinding.txtLocation.text =
+                                data.address.country + " , " + data.address.city
+                            viewModel.getSolahWithLatLog(
+                                SendParam(
+                                    latitude = data.lat.toDouble(),
+                                    longitude = data.lon.toDouble(),
+                                    method = 3,
+                                    month = 6,
+                                    year = 2021
+                                )
+                            )
+                        }
+                        ResultRequest.Status.LOADING -> {
+                        }
+                        ResultRequest.Status.ERROR -> {
+                        }
+                    }
                 }
-                ResultRequest.Status.LOADING->{}
-                ResultRequest.Status.ERROR->{}
-            }
-            }
 
-        }
+                CoroutineScope(Dispatchers.IO).launch {
+                    viewModel.getNextTimeLiveData.collect {
+                        withContext(Dispatchers.Main) {
+                            when (it.status) {
+                                ResultRequest.Status.EMPTY -> {
+                                    // mBinding.time.nextTime.text = "${it.data}"
+
+                                }
+                                ResultRequest.Status.SUCCESS -> {
+                                    val data = it.data as String
+                                    mBinding.time.text = data
+
+                                }
+                                ResultRequest.Status.LOADING -> {
+                                }
+                                ResultRequest.Status.ERROR -> {
+                                }
+                            }
+                        }
+
+                    }
+                }
+                CoroutineScope(Dispatchers.IO).launch {
+                    viewModel.getSolahWithLatLogLiveData.collect {
+                        withContext(Dispatchers.Main) {
+                            when (it.status) {
+                                ResultRequest.Status.EMPTY -> {
+                                }
+                                ResultRequest.Status.SUCCESS -> {
+                                    Log.e(
+                                        this.javaClass.name,
+                                        "getSolahWithLatLogLiveData SUCCESS"
+                                    )
+
+                                    val data = it.data as SolahTime
+                                    val solahArray =
+                                        addSolah(requireContext(), data.data.first().timings)
+                                    viewModel.getNextTime(solahArray)
+
+                                }
+                                ResultRequest.Status.LOADING -> {
+                                    Log.e(
+                                        this.javaClass.name,
+                                        "getSolahWithLatLogLiveData LOADING"
+                                    )
+                                }
+                                ResultRequest.Status.ERROR -> {
+                                    Log.e(this.javaClass.name, it.message!!)
+
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+            }
         }
     }
 }
--- app/src/main/java/com/ps/omarmattr/qalby/ui/fragment/SolahFragment.kt
+++ app/src/main/java/com/ps/omarmattr/qalby/ui/fragment/SolahFragment.kt
@@ -14,6 +14,7 @@ import com.ps.omarmattr.qalby.databinding.FragmentSolahBinding
 import com.ps.omarmattr.qalby.model.location.ResultLocation
 import com.ps.omarmattr.qalby.model.solahTime.*
 import com.ps.omarmattr.qalby.model.solahTime.Date
+import com.ps.omarmattr.qalby.other.FunctionConstant.addSolah
 import com.ps.omarmattr.qalby.ui.viewmodel.MainViewModel
 import com.ps.omarmattr.qalby.ui.viewmodel.SolahViewModel
 import com.ps.omarmattr.qalby.util.ResultRequest
@@ -59,35 +60,7 @@ class SolahFragment : Fragment(), GenericAdapter.OnListItemViewClickListener<Sol
                 adapter = mAdapter
             }
         }
-        CoroutineScope(Dispatchers.IO).launch {
-            mainViewModel.getLocationLiveData.collect {
-                withContext(Dispatchers.Main) {
-                    when (it.status) {
-                        ResultRequest.Status.EMPTY -> {
-                        }
-                        ResultRequest.Status.SUCCESS -> {
-                            val data = it.data as ResultLocation
-                            Log.e(this@SolahFragment.javaClass.name, "getLocationLiveData")
 
-                            viewModel.getSolahWithLatLog(
-                                SendParam(
-                                    latitude = data.lat.toDouble(),
-                                    longitude = data.lon.toDouble(),
-                                    method = 3,
-                                    month = 6,
-                                    year = 2021
-                                )
-                            )
-                        }
-                        ResultRequest.Status.LOADING -> {
-                        }
-                        ResultRequest.Status.ERROR -> {
-                        }
-                    }
-                }
-
-            }
-        }
         CoroutineScope(Dispatchers.IO).launch {
             viewModel.getNextTimeLiveData.collect {
                 withContext(Dispatchers.Main) {
@@ -125,7 +98,7 @@ class SolahFragment : Fragment(), GenericAdapter.OnListItemViewClickListener<Sol
                             val data = it.data as SolahTime
 
                             setUITimes(data.data.first().date)
-                            val solahArray = addSolah(data.data.first().timings)
+                            val solahArray = addSolah(requireContext(),data.data.first().timings)
                             mAdapter.data = solahArray
                             viewModel.getNextTime(solahArray)
 
@@ -151,57 +124,11 @@ class SolahFragment : Fragment(), GenericAdapter.OnListItemViewClickListener<Sol
 
     }
 
-    private fun addSolah(timings: Timings): ArrayList<SolahItem> {
-        return arrayListOf(
-            SolahItem(
-                name = getString(R.string.fajr),
-                time = time24to12(timings.fajr),
-                state = false
-            ),
-            SolahItem(
-                name = getString(R.string.sunless),
-                time = time24to12(timings.sunrise),
-                state = false
-            ), SolahItem(
-                name = getString(R.string.dhuhr),
-                time = time24to12(timings.dhuhr),
-                state = false
-            ), SolahItem(
-                name = getString(R.string.asr),
-                time = time24to12(timings.asr),
-                state = false
-            ), SolahItem(
-                name = getString(R.string.maghrib),
-                time = time24to12(timings.maghrib),
-                state = false
-            ), SolahItem(
-                name = getString(R.string.sunset),
-                time = time24to12(timings.sunset),
-                state = false
-            ), SolahItem(
-                name = getString(R.string.isha),
-                time = time24to12(timings.isha),
-                state = false
-            )
-        )
-    }
-
     private fun setUITimes(date: Date) {
         mBinding.day.text = date.gregorian.weekday.en
         mBinding.fullDate.text = date.readable
         mBinding.islamicDate.text = date.hijri.date
 
-    }
-
-    fun time24to12(time: String): String {
-        return try {
-            val sdf = SimpleDateFormat("H:mm", Locale.getDefault())
-            val dateObj = sdf.parse(time)
-            SimpleDateFormat("hh:mm a", Locale.getDefault()).format(dateObj!!)
-        } catch (e: ParseException) {
-            e.printStackTrace()
-            time
-        }
     }
 
 }
