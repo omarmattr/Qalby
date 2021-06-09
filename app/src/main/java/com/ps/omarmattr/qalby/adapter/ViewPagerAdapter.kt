578b305dec33bfaa82e6d8cc763af10141b3d1dc
OMEN <mrh61037@gmail.com>
2021-06-09 14:21:44 +0300

    update view pager adapter

--- app/src/main/java/com/ps/omarmattr/qalby/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/ps/omarmattr/qalby/adapter/ViewPagerAdapter.kt
@@ -1,30 +1,57 @@
 package com.ps.omarmattr.qalby.adapter
 
+import android.app.FragmentManager
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
-
-class ViewPagerAdapter(fragment: FragmentActivity)
-    : FragmentStateAdapter(fragment) {
-
-    private val lf = ArrayList<Fragment>()
-    private val lt = ArrayList<String>()
+import androidx.fragment.app.FragmentPagerAdapter
+import javax.annotation.Nullable
 
 
-    override fun getItemCount(): Int {
+//class ViewPagerAdapter(fragment: FragmentActivity)
+//    : FragmentStateAdapter(fragment) {
+//
+//    private val lf = ArrayList<Fragment>()
+//    private val lt = ArrayList<String>()
+//
+//
+//    override fun getItemCount(): Int {
+//        return lf.size
+//    }
+//
+//    override fun createFragment(position: Int): Fragment {
+//        return lf[position]
+//    }
+//
+//    fun addFragment(fragment: Fragment?, title: String?) {
+//        lf.add(fragment!!)
+//        lt.add(title!!)
+//    }
+//
+//
+//
+//
+//}
+
+
+class ViewPagerAdapter( fm: androidx.fragment.app.FragmentManager) :
+    FragmentPagerAdapter(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    var lf: ArrayList<Fragment> = ArrayList()
+    var lt: ArrayList<String> = ArrayList()
+
+    fun addFragment(fragment: Fragment, title: String) {
+        lf.add(fragment)
+        lt.add(title)
+    }
+
+    override fun getCount(): Int {
         return lf.size
     }
 
-    override fun createFragment(position: Int): Fragment {
+    override fun getItem(position: Int): Fragment {
         return lf[position]
     }
 
-    fun addFragment(fragment: Fragment?, title: String?) {
-        lf.add(fragment!!)
-        lt.add(title!!)
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return lt[position]
     }
-
-
-
-
 }
